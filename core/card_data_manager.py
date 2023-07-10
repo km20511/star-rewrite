10634cbba0d1484d6facad65d2db25aa83ec6281
km20511 <gkm20511@gmail.com>
2023-07-10 14:22:20 +0900

    카드 데이터 접근 함수 구현, /src/core -> /core로 파일 구조 개편

---
+++ core/card_data_manager.py
@@ -0,0 +1,97 @@
+"""
+카드와 아이템 데이터가 기록된 json 파일에 접근하고 python 자료구조로 파싱하는 스크립트.
+"""
+import json
+from enum import Enum
+from typing import List
+from dataclasses import dataclass
+
+
+class CardType(Enum):
+    """카드의 유형. 적, 아이템, 사건 카드로 구분."""
+    Enemy = 0
+    Item = 1
+    Event = 2
+
+
+@dataclass(frozen=True)
+class CardData:
+    """카드의 속성을 담는 자료구조."""
+    id: int
+    name: str
+    type: CardType
+    cost: int
+    sprite_name: str
+    description: str
+    effects: List[dict]
+
+
+@dataclass(frozen=True)
+class ItemData:
+    """아이템의 속성을 담는 자료구조."""
+    id: int
+    name: str
+    sprite_name: str
+    description: str
+    effects: List[dict]
+
+
+CARDS_DATA_PATH = "data/cards/cards.json"
+"""카드 데이터 파일의 경로. 상수이므로 수정하지 말 것."""
+
+ITEMS_DATA_PATH = "data/cards/items.json"
+"""아이템 데이터 파일의 경로. 상수이므로 수정하지 말 것."""
+
+__db_cards: List[CardData] = []
+"""등록된 카드 데이터의 목록. 외부에서 접근하지 말 것. (대신 get_card_data()를 사용할 것.)"""
+
+__db_items: List[ItemData] = []
+"""등록된 아이템 데이터의 목록. 외부에서 접근하지 말 것. (대신 get_item_data()를 사용할 것.)"""
+
+def initialize():
+    """파일을 불러오고 DB를 초기화하는 함수. 이 모듈을 사용하기 전 호출할 것."""
+    __db_cards.clear()
+    __db_items.clear()
+
+    with open(CARDS_DATA_PATH, encoding = "utf-8") as f:
+        tree_cards: dict = json.load(f)
+
+    for card in tree_cards["contents"]:
+        __db_cards.append(CardData(
+            card["id"],
+            card["name"],
+            CardType[card["type"]],
+            card["cost"],
+            card["sprite_name"],
+            card["description"],
+            card["effects"]
+        ))
+
+    with open(ITEMS_DATA_PATH, encoding = "utf-8") as f:
+        tree_items: dict = json.load(f)
+    
+    for item in tree_items["contents"]:
+        __db_items.append(ItemData(
+            item["id"],
+            item["name"],
+            item["sprite_name"],
+            item["description"],
+            item["effects"]
+        ))
+
+def get_card_data(id: int) -> CardData:
+    """DB에서 주어진 id에 해당하는 카드를 찾아 반환. 찾지 못할 경우 None 반환."""
+    for i in __db_cards:
+        if i.id == id:
+            return i
+    return None
+
+def get_item_data(id: int) -> ItemData:
+    """DB에서 주어진 id에 해당하는 아이템을 찾아 반환. 찾지 못할 경우 None 반환."""
+    for i in __db_items:
+        if i.id == id:
+            return i
+    return None
+
+if __name__ == "__main__":
+    initialize()
